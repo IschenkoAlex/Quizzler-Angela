d263cf31d909bf320c6fab5135f23132546d67c2
Alexander Ischenko <ischeenko@gmail.com>
2019-10-21 15:06:28 +0100

    Update some VC

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,6 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    private lazy var trueButton: UIButton = {
+        var button = UIButton()
+        button.setTitle("True", for: .normal)
+        button.setBackgroundImage(UIImage(named: "rectangle"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private lazy var falseButton: UIButton = {
+        var button = UIButton()
+        button.setTitle("False", for: .normal)
+        button.setBackgroundImage(UIImage(named: "rectangle"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private lazy var progressBar: UIProgressView = {
+        var progressView = UIProgressView()
+        progressView.progress = 0.5
+        progressView.tintColor = UIColor(red: 0.255, green: 0.117, blue: 0.168, alpha: 1)
+        progressView.trackTintColor = .white
+        progressView.translatesAutoresizingMaskIntoConstraints = false
+        return progressView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
